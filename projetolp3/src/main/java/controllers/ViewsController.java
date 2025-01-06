1474b378635dd2f355465190286283ab69731f56
SamuelLemos20 <1231557@isep.ipp.pt>
2025-01-06 18:46:45 +0000

    Correção de bug no login(comentar método teste)

--- projetolp3/src/main/java/controllers/ViewsController.java
+++ projetolp3/src/main/java/controllers/ViewsController.java
@@ -106,7 +106,7 @@ public class ViewsController {
             boolean sucess = loginController.loginVerify(id, password, event);
             System.out.println(id);
             if (sucess) {
-                teste(id);
+//                teste(id);
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("Sucesso!");
                 alert.setHeaderText("Login efetuado com sucesso!");
